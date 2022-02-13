f20b7ece0417777bf2fcfd05a3bad7b0cd810c1c
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2022-02-13 15:12:58 -0300

    challenge finished

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,15 +30,23 @@ export function Home() {
 
   async function loadData() {
     const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+    const response = await AsyncStorage.getItem(dataKey);
+    if (response) {
+      const listData = JSON.parse(response);
+      setSearchListData(listData);
+      setData(listData);
+    }
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    if(searchText){
+      const dataFiltered = data.filter((item) => item.service_name === searchText);
+      setSearchListData(dataFiltered);
+    }
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    setSearchText(text);
   }
 
   useFocusEffect(useCallback(() => {
